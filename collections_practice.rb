94a0e4c85a479c475ff0ccaef4bfcea5104d1144
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-17 09:14:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,5 +45,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| element[2] ? element : element << "s"}
+  array.each_with_index.collect {|element, index| element == "feet" ? element : element << "s"}
 end
