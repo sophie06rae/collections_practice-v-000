bf120270a6734f1fafdc4c47d0c5ef301da19e1a
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-17 08:15:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,7 +16,13 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a, b|
-    if a.length > b.length
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    else a.length > b.length
       1
     end 
+
+    end
 end
