82ea619c130d7305ddaad115871d460801c22958
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-17 08:52:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,5 +45,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| if element[index] == 2 element else element << "s"}
+  array.each_with_index.collect {|element, index| if element[index] == 2 element element << "s" end}
 end
